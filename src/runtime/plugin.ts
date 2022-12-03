21bc1d7f7b8bf913ff31f9957495ae312f2371d2
Nikita Madebeykin <nikita.madebeykin@gmail.com>
2022-12-03 22:33:30 +0400

    fix: remove page title from hits

--- src/runtime/plugin.ts
+++ src/runtime/plugin.ts
@@ -39,12 +39,11 @@ export default defineNuxtPlugin(async ({ _ }) => {
         return
       }
       if (consoleLog) {
-        console.log(`Yandex Metrika page hit: "${to.fullPath}" (referer="${from.fullPath}", title="${to.meta.title}")`)
+        console.log(`Yandex Metrika page hit: "${to.fullPath}" (referer="${from.fullPath}")`)
       }
       if (!isDev) {
         ym(id, 'hit', to.fullPath, {
-          referer: /* basePath + */from.fullPath,
-          title: to.meta.title
+          referer: /* basePath + */from.fullPath
         })
       }
     })
